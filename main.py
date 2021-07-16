0064634aa0efed462f18fa06629293857bb83dcd
Wojciech-Mi <85784870+Wojciech-Mi@users.noreply.github.com>
2021-07-16 23:12:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
-from checkers.board import Board
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+from checkers.game import Game
 
 
 FPS = 60
@@ -17,11 +17,15 @@ def get_row_col_from_mouse(pos):
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
 
     while run:
         clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,13 +34,9 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                piece = board.get_piece(row, col)
-                board.move(piece, 4, 3)
+                game.select(row, col)
 
-
-        board.draw(WIN)
-        pygame.display.update()
-
+        game.update()
 
     pygame.quit()
 
